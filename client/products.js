4586620f6dcf1faf6af94e4646ac68b7ce515c3d
Sean Pollock <seanpollock3344@gmail.com>
2017-05-19 09:16:09 -0500

    adding local store of products and dates covered

---
+++ client/products.js
@@ -0,0 +1,181 @@
+export const Products = [{
+  name: 'Brent Crude Oil Financial Futures',
+  symbol: 'BZ'
+}, {
+  name: 'Brent Crude Oil Futures',
+  symbol: 'BB'
+}, {
+  name: 'Canadian Heavy Crude Oil BALMO Futures',
+  symbol: 'CHB'
+}, {
+  name: 'Crude Oil Financial Futures',
+  symbol: 'WS'
+}, {
+  name: 'E-Mini Crude Oil Futures',
+  symbol: 'QM'
+}, {
+  name: 'Crude Oil Futures',
+  symbol: 'CL'
+}, {
+  name: 'LLS (Angus) vs. Brent Balmo Futures',
+  symbol: 'LLB'
+}, {
+  name: 'LLS (Angus) vs. WTI Balmo Futures',
+  symbol: 'LWB'
+}, {
+  name: 'LLS (Angus) vs. WTI Financial Futures',
+  symbol: 'WJ'
+}, {
+  name: 'WTI Financial Futures',
+  symbol: 'CS'
+}, {
+  name: 'WTI-Brent Financial Futures',
+  symbol: 'BK'
+}]
+
+export const Dates = [
+  {
+    'month': '2017-01-01'
+  },
+  {
+    'month': '2017-02-01'
+  },
+  {
+    'month': '2017-03-01'
+  },
+  {
+    'month': '2017-04-01'
+  },
+  {
+    'month': '2017-05-01'
+  },
+  {
+    'month': '2017-06-01'
+  },
+  {
+    'month': '2017-07-01',
+  },
+  {
+    'month': '2017-08-01',
+  },
+  {
+    'month': '2017-09-01',
+  },
+  {
+    'month': '2017-10-01',
+  },
+  {
+    'month': '2017-11-01',
+  },
+  {
+    'month': '2017-12-01',
+  },
+  {
+    'month': '2018-01-01'
+  },
+  {
+    'month': '2018-02-01'
+  },
+  {
+    'month': '2018-03-01'
+  },
+  {
+    'month': '2018-04-01'
+  },
+  {
+    'month': '2018-05-01'
+  },
+  {
+    'month': '2018-06-01'
+  },
+  {
+    'month': '2018-07-01',
+  },
+  {
+    'month': '2018-08-01',
+  },
+  {
+    'month': '2018-09-01',
+  },
+  {
+    'month': '2018-10-01',
+  },
+  {
+    'month': '2018-11-01',
+  },
+  {
+    'month': '2018-12-01',
+  },
+  {
+    'month': '2019-01-01'
+  },
+  {
+    'month': '2019-02-01'
+  },
+  {
+    'month': '2019-03-01'
+  },
+  {
+    'month': '2019-04-01'
+  },
+  {
+    'month': '2019-05-01'
+  },
+  {
+    'month': '2019-06-01'
+  },
+  {
+    'month': '2019-07-01',
+  },
+  {
+    'month': '2019-08-01',
+  },
+  {
+    'month': '2019-09-01',
+  },
+  {
+    'month': '2019-10-01',
+  },
+  {
+    'month': '2019-11-01',
+  },
+  {
+    'month': '2019-12-01',
+  },
+  {
+    'month': '2020-01-01'
+  },
+  {
+    'month': '2020-02-01'
+  },
+  {
+    'month': '2020-03-01'
+  },
+  {
+    'month': '2020-04-01'
+  },
+  {
+    'month': '2020-05-01'
+  },
+  {
+    'month': '2020-06-01'
+  },
+  {
+    'month': '2020-07-01',
+  },
+  {
+    'month': '2020-08-01',
+  },
+  {
+    'month': '2020-09-01',
+  },
+  {
+    'month': '2020-10-01',
+  },
+  {
+    'month': '2020-11-01',
+  },
+  {
+    'month': '2020-12-01',
+  },
+]
